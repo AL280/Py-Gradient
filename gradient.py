6c6327523091e117adf30a838f34eaa9fcfcad5c
AL <98286536+AL280@users.noreply.github.com>
2022-09-14 20:02:44 +0300

    Add files via upload

---
+++ gradient.py
@@ -0,0 +1,15 @@
+
+from pystyle import *
+from colorama import Back, Fore, Style
+
+
+banner = r'''
+ █████╗ ██╗     
+██╔══██╗██║     
+███████║██║     
+██╔══██║██║     
+██║  ██║███████╗
+╚═╝  ╚═╝╚══════╝                
+'''
+System.Clear()
+Anime.Fade(Center.Center(banner), Colors.purple_to_blue, Colorate.Vertical, interval=0.030, enter=True) # you can change purple_to_blue to whatever u want the gradient to be
